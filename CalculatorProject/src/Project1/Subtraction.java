4932256753f0b0829ab8b1814d93b0f60ae2f7b5
PRASANTH M <151944542+gitmprasanth@users.noreply.github.com>
2025-05-05 12:36:38 +0530

    Update Subtraction.java

--- CalculatorProject/src/Project1/Subtraction.java
+++ CalculatorProject/src/Project1/Subtraction.java
@@ -3,7 +3,7 @@ package Project1;
 public class Subtraction {
    public void subtraction(int[]arr) {
 	   int sub=arr[0];
-	   for(int i=0;i<arr.length;i++) {
+	   for(int i=1;i<arr.length;i++) {
 		   sub-=arr[i];
 	   }
 	   System.out.println("Subtraction is "+sub);
